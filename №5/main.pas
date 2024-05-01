98ea45c0bd58e722cfcfd909f3eea54423737be4
matthewqx13 <144227151+matthewqx13@users.noreply.github.com>
2024-05-01 22:31:44 +0300

    Add files via upload

---
+++ №5/main.pas
@@ -0,0 +1,117 @@
+﻿type
+  Comparator = function(a, b: integer): boolean;
+
+function AComparator(a, b: integer): boolean;
+begin
+  AComparator := a < b;
+end;
+
+function BComparator(a, b: integer): boolean;
+begin
+  BComparator := a > b;
+end;
+
+procedure SelectionSort(var arr: array of integer; comparator: Comparator);
+var
+  i, j, minIdx, temp: integer;
+begin
+  for i := 0 to Length(arr) - 2 do
+  begin
+    minIdx := i;
+    for j := i + 1 to Length(arr) - 1 do
+    begin
+      if comparator(arr[j], arr[minIdx]) then
+        minIdx := j;
+    end;
+    temp := arr[minIdx];
+    arr[minIdx] := arr[i];
+    arr[i] := temp;
+  end;
+end;
+
+procedure Heapify(var arr: array of integer; n, i: integer; comparator: Comparator);
+var
+  largest, left, right: integer;
+begin
+  largest := i;
+  left := 2 * i + 1;
+  right := 2 * i + 2;
+
+  if (left < n) and comparator(arr[left], arr[largest]) then
+    largest := left;
+
+  if (right < n) and comparator(arr[right], arr[largest]) then
+    largest := right;
+
+  if largest <> i then
+  begin
+    Swap(arr[i], arr[largest]);
+    Heapify(arr, n, largest, comparator);
+  end;
+end;
+
+procedure HeapSort(var arr: array of integer; comparator: Comparator);
+var
+  i: integer;
+begin
+  for i := Length(arr) div 2 - 1 downto 0 do
+    Heapify(arr, Length(arr), i, comparator);
+
+  for i := Length(arr) - 1 downto 1 do
+  begin
+    Swap(arr[0], arr[i]);
+    Heapify(arr, i, 0, comparator);
+  end;
+end;
+
+procedure ReadDataFromFile(var arr: array of integer; filename: string);
+var
+  fileData: TextFile;
+  i: integer;
+begin
+  AssignFile(fileData, filename);
+  Reset(fileData);
+
+  i := 0;
+  while not Eof(fileData) do
+  begin
+    Readln(fileData, arr[i]);
+    Inc(i);
+  end;
+
+  CloseFile(fileData);
+end;
+
+procedure WriteDataToFile(arr: array of integer; filename: string);
+var
+  fileData: TextFile;
+  i: integer;
+begin
+  AssignFile(fileData, filename);
+  Rewrite(fileData);
+
+  for i := 0 to Length(arr) - 1 do
+    Writeln(fileData, arr[i]);
+
+  CloseFile(fileData);
+end;
+
+var
+  data: array of integer;
+  i: integer; 
+begin
+  SetLength(data, 10);
+  ReadDataFromFile(data, 'input.txt');
+  Writeln('Данные до сортировки:');
+  for i := 0 to Length(data) - 1 do
+    Write(data[i], ' ');
+  Writeln;
+
+  SelectionSort(data, AComparator);
+  Writeln('Данные после сортировки:');
+  for i := 0 to Length(data) - 1 do
+    Write(data[i], ' ');
+  Writeln;
+
+  WriteDataToFile(data, 'output.txt');
+end.
---
+++ №6/main.pas
@@ -0,0 +1,118 @@
+﻿type
+  Node = record
+    data: integer;
+    next: integer;
+  end;
+
+var
+  list: array[1..10] of Node;
+  head, freeIndex: integer;
+
+procedure InitializeList;
+var
+  i: integer;
+begin
+  for i := 1 to 10 do
+  begin
+    list[i].next := i + 1;
+  end;
+  list[10].next := 0; // Последний элемент списка
+  head := 0; // Голова списка
+  freeIndex := 1; // Начальный свободный индекс
+end;
+
+procedure DisplayList;
+var
+  current: integer;
+begin
+  current := head;
+  writeln('Содержимое списка:');
+  while current <> 0 do
+  begin
+    writeln(list[current].data);
+    current := list[current].next;
+  end;
+end;
+
+procedure AddNode(data: integer);
+var
+  newIndex, current: integer;
+begin
+  if freeIndex <> 0 then
+  begin
+    newIndex := freeIndex;
+    freeIndex := list[freeIndex].next;
+
+    list[newIndex].data := data;
+    list[newIndex].next := 0;
+
+    if head = 0 then
+      head := newIndex
+    else
+    begin
+      current := head;
+      while list[current].next <> 0 do
+        current := list[current].next;
+      list[current].next := newIndex;
+    end;
+  end
+  else
+    writeln('Список полон, невозможно добавить новый элемент.');
+end;
+
+procedure DeleteNode(data: integer);
+var
+  current, prev: integer;
+begin
+  current := head;
+  prev := 0;
+
+  while (current <> 0) and (list[current].data <> data) do
+  begin
+    prev := current;
+    current := list[current].next;
+  end;
+
+  if current <> 0 then
+  begin
+    if prev = 0 then
+      head := list[current].next
+    else
+      list[prev].next := list[current].next;
+    list[current].next := freeIndex;
+    freeIndex := current;
+  end
+  else
+    writeln('Элемент не найден.');
+end;
+
+var
+  choice, data: integer;
+
+begin
+  InitializeList;
+
+  repeat
+    writeln('Меню:');
+    writeln('1. Вывести список');
+    writeln('2. Добавить элемент');
+    writeln('3. Удалить элемент');
+    writeln('0. Выход');
+    write('Выберите действие: ');
+    readln(choice);
+
+    case choice of
+      1: DisplayList;
+      2: begin
+           write('Введите данные для нового элемента: ');
+           readln(data);
+           AddNode(data);
+         end;
+      3: begin
+           write('Введите данные элемента для удаления: ');
+           readln(data);
+           DeleteNode(data);
+         end;
+    end;
+  until choice = 0;
+end.
